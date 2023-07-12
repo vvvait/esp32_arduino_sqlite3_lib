e6f936f0c00eba84079cd9cf5d408db85871a40c
vvvait <47783199+vvvait@users.noreply.github.com>
2023-07-12 18:14:00 +0300

    no_mem_func

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -159,6 +159,7 @@ extern "C" {
 **
 ** See also: [sqlite_version()] and [sqlite_source_id()].
 */
+SQLITE_API SQLITE_EXTERN char no_mem_func[64];
 SQLITE_API SQLITE_EXTERN const char sqlite3_version[];
 SQLITE_API const char *sqlite3_libversion(void);
 SQLITE_API const char *sqlite3_sourceid(void);
